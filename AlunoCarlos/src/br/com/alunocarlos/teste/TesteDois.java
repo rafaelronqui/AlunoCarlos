30d8bc4984f78b9613b83baf5b6ee6152ff4efe3
rafaelronqui <rafaelmronqui@gmail.com>
2023-06-02 20:33:57 -0300

    Teste dois.

---
+++ AlunoCarlos/src/br/com/alunocarlos/teste/TesteDois.java
@@ -0,0 +1,10 @@
+package br.com.alunocarlos.teste;
+
+public class TesteDois {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Teste dois");
+	}
+
+}
